6c3e14a24d570edfb1a1d870759343c3f28569c2
Stephen L. De Rudder <sldr@users.noreply.github.com>
2023-04-02 03:25:24 -0500

    Update Write-VerboseEx.ps1 with help.

--- Write-VerboseEx.ps1
+++ Write-VerboseEx.ps1
@@ -1,5 +1,28 @@
 Function Write-VerboseEx
 {
+<#
+.SYNOPSIS
+    An extended Write-Verbose that supports time stamping and showing the caller in the message.
+
+.DESCRIPTION
+    An extended Write-Verbose that supports time stamping and showing the caller in the message.
+
+.Parameter Message
+    Specifies the message to use.
+
+.Parameter Timestamp
+    Specifies that the time stampe should be used. The default is $true (use -Timestamp:$false to disable).
+
+.Parameter Caller
+    Shows the function name that called Write-VerboseEx. If this was called from outside a function then the string "<ScriptBlock>" will be used instead. The default is $false.
+
+.EXAMPLE
+    PS > Write-VerboseEx -Timestamp -Caller "Hello World!"
+    VERBOSE: 2022-04-22T02:44:22.6195663-05:00:<ScriptBlock>: Hello World!
+
+    This produced a verbose message with a timestamp and caller.
+#>
+
     param(
         [Parameter(Mandatory=$true, Position=0, ValueFromPipeline=$true)]
         [Alias('Msg')]
